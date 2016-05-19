af0834ea4fca62fc676fa9deefd4bdc0091bb452
michi <michi@localhost>
2016-05-19 20:01:19 +0200

    Added structure dump in json format

--- Souliss.py
+++ Souliss.py
@@ -1,6 +1,7 @@
 from socket import *
 from Macaco_frame import Macaco_frame
 from VNet_frame import VNet_frame
+import json
 
 RESPONSE = 0x10
 
@@ -24,52 +25,74 @@ SOULISS_FC_DISCOVER_GATEWAY_REQUEST = 0x28              # TODO
 
 # TODO: Dynamic network configuration
 
-typical = {
-    0x11:"ON/OFF Salida digital con timer",
-    0x12:"ON/OFF Salida con modo AUT",
-    0x13:"Valor de entrada digital",
-    0x14:"Pulso de salida digital",
-    0x16:"Tira de LED RGB",
-    0x18:"ON/OFF Salida Digital",
-    0x19:"Luz Regulable",
-    0x19:"Brillo actual:",
-    0x19:"Configurar brillo a:",
-    0x21:"Dispositivo motorizado (con final de carrera)",
-    0x22:"Dispositivo motorizado",
-    0x31:"Termostato",
-    0x41:"Sistema antirobo",
-    0x42:"Peer antirobo",
-    0x51:"Entrada analogica",
-    0x52:"Entrada analogica, valor medio",
-    0x53:"Entrada analogica, interactividad maxima",
-    0x54:"Sensor de luz",
-    0x55:"Sensor de Voltaje",
-    0x56:"Sensor de Corriente",
-    0x57:"Sensor de Potencia",
-    0x58:"Sensor de Presion",
-    0x61:"Analog setpoint, half-precision floating point",
-    0x62:"Temperature setpoint (-20, +50)C",
-    0x63:"Humidity setpoint (0, 100)%",
-    0x64:"Light setpoint (0, 40) kLux",
-    0x65:"Voltage setpoint(0, 400) V",
-    0x66:"Current setpoint(0, 25) A",
-    0x67:"Power setpoint(0, 6500) W",
-    0x68:"Pressure setpoint (0, 1500) hPa"
-    }
+typical_types = {
+        0x11: {"desc" : "Typical 11 : ON/OFF Digital Output with Timer Option", "size": 1},
+        0x12: {"desc" : "Typical 12 : ON/OFF Digital Output with AUTO mode", "size": 1},
+        0x13: {"desc" : "Typical 13 : Digital Input Value", "size": 1},
+        0x14: {"desc" : "Typical 14 : Pulse Digital Output", "size": 1},
+        0x15: {"desc" : "Typical 15 : RGB Light", "size": 2},
+        0x16: {"desc" : "Typical 16 : RGB LED Strip", "size": 4},
+        0x18: {"desc" : "Typical 18 : ON/OFF Digital Output (Step Relay)", "size": 1},
+        0x19: {"desc" : "Typical 19 : Single Color LED Strip", "size": 2},
+        0x1A: {"desc" : "Typical 1A : Digital Input Pass Through", "size": 1},
+        0x1B: {"desc" : "Typical 1B : Position Constrained ON/OFF Digital Output", "size": 1},
+        0x21: {"desc" : "Typical 21 : Motorized devices with limit switches", "size": 1},
+        0x22: {"desc" : "Typical 22 : Motorized devices with limit switches and middle position", "size": 1},
+        0x31: {"desc" : "Typical 31 : Temperature control with cooling and heating mode", "size": 5},
+        0x32: {"desc" : "Typical 32 : Air Conditioner", "size": 2},
+        0x41: {"desc" : "Typical 41 : Anti-theft integration -Main-",  "size": 1},
+        0x42: {"desc" : "Typical 42 : Anti-theft integration -Peer-",  "size": 1},
+        0x51: {"desc" : "Typical 51 : Analog input, half-precision floating point", "size": 2},
+        0x52: {"desc" : "Typical 52 : Temperature measure (-20, +50) C", "size": 2},
+        0x53: {"desc" : "Typical 53 : Humidity measure (0, 100) " , "size": 2},
+        0x54: {"desc" : "Typical 54 : Light Sensor (0, 40) kLux", "size": 2},
+        0x55: {"desc" : "Typical 55 : Voltage (0, 400) V", "size": 2},
+        0x56: {"desc" : "Typical 56 : Current (0, 25) A", "size": 2},
+        0x57: {"desc" : "Typical 57 : Power (0, 6500) W", "size": 2},
+        0x58: {"desc" : "Typical 58 : Pressure measure (0, 1500) hPa", "size": 2},
+        0x61: {"desc" : "Typical 61 : Analog setpoint, half-precision floating point", "size": 2},
+        0x62: {"desc" : "Typical 62 : Temperature measure (-20, +50) C", "size": 2},
+        0x63: {"desc" : "Typical 63 : Humidity measure (0, 100) ", "size": 2},
+        0x64: {"desc" : "Typical 64 : Light Sensor (0, 40) kLux", "size": 2},
+        0x65: {"desc" : "Typical 65 : Voltage (0, 400) V", "size": 2},
+        0x66: {"desc" : "Typical 66 : Current (0, 25) A", "size": 2},
+        0x67: {"desc" : "Typical 67 : Power (0, 6500) W", "size": 2},
+        0x68: {"desc" : "Typical 68 : Pressure measure (0, 1500) hPa", "size": 2}
+        }
+
 
 class Typical:
-    def __init__(self, type):
-        self.type = type
-        self.description = typical[type]
+    def __init__(self, ttype):
+        self.ttype = ttype
+        self.description = typical_types[ttype]['desc']
+        self.size = typical_types[ttype]['size']
+        self.slot = -1 # undefined until added to a node
+
+    def set_slot(self, slot):
+        self.slot = slot
+
+    def to_dict(self):
+        return {'ddesc': self.description,
+                'slo': self.slot,
+                'typ': self.ttype}
+
         
 class Node:
     def __init__(self, i):
         self.index = i
         self.typicals = []
+        self.next_slot = 0
         pass
 
     def add_typical(self, typical):
+        typical.set_slot(self.next_slot)
         self.typicals.append(typical)
+        self.next_slot = self.next_slot + typical.size
+
+    def to_dict(self):
+        return {'ndesc': 'Nodo ' + str(self.index),
+                'slot': [t.to_dict() for t in self.typicals]}
+
 
 class Souliss:
 
@@ -94,7 +117,7 @@ class Souliss:
         return self.get_response(SOULISS_FC_PING) != False
 
     def log(self, msg):
-        if self.log:
+        if self.logging:
             print "LOG: %s" % msg
 
     """
@@ -165,6 +188,12 @@ class Souliss:
             print "Nodo %d de %d" % (n.index+1, len(self.nodes))
             for t in n.typicals:
                 print "   Tipo: %d - %s" % (t.type, t.description)
+        
+    def dump_json_structure(self):
+        res = {'id': []}
+        for n in self.nodes:
+            res['id'].append(n.to_dict())
+        print json.dumps(res, indent=4)
 
     def close(self):
         self.socket.close()
